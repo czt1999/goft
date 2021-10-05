48812d6dd01f2bd2d30a6915a08caf004a3dc1f5
cztian0x7f <2315096491@qq.com>
2021-10-05 20:57:19 +0800

    add "main/uintptr.go"

---
+++ main/uintptr.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+type sample struct {
+	a int
+	b string
+}
+
+func main() {
+	s := &sample{a: 1, b: "hello"}
+
+	// Getting the address of field b in struct s
+	p := unsafe.Pointer(uintptr(unsafe.Pointer(s)) + unsafe.Offsetof(s.b))
+
+	// Typecasting it to a string pointer and printing the value of it
+	fmt.Println(*(*string)(p))
+}
